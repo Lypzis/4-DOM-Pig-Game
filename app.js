c4821696df2bd0d6f55c15e87a01665657d13494
Victor_Piccoli <victorvp2008@hotmail.com>
2018-02-07 15:37:09 -0200

    Update app.js

--- app.js
+++ app.js
@@ -21,27 +21,52 @@ GAME RULES:
 
 var scores,
     roundScore,
-    activePlayer,
-    dice;
+    activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor((Math.random() * 6)+1);
-console.log(dice);
-
-document.querySelector('#score-0').textContent = scores[0];
-document.querySelector('#score-1').textContent = scores[1];
-
-//setter
-document.querySelector('#current-' + activePlayer).textContent = dice; //it can only receive plain text
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; //capable of understand html tags
-
-//getter
-var x = document.querySelector('#score-1').textContent;
-console.log(x);
+document.getElementById('score-0').textContent = scores[0];
+document.getElementById('score-1').textContent = scores[1];
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //1. Random Number;
+    var dice = Math.floor((Math.random() * 6)+1);
+
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. Update the round score IF the rolled number was not 1
+    var currentPlayerScore = document.querySelector('#current-' + activePlayer);
+    if (dice !== 1){
+        //add score
+        roundScore += dice;
+        currentPlayerScore.textContent = roundScore;
+    } else {
+        roundScore = 0;
+        currentPlayerScore.textContent = roundScore;
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    
+        //next player
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');  
+
+        diceDOM.style.display = 'none';
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    //1. get the current player score
+
+    //2. add current score to the active player score
+
+    //3. next player
+});
 
